fd634b013d9c76fe6dce075fbc7149fab51d9f51
sanfengliao <1164406557@qq.com>
2019-08-06 21:36:43 +0800

    使用路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Rank from "../components/rank/rank.vue";
-import Recommend from "../components/recommend/recomend.vue";
-import Search from "../components/search/search.vue";
-import Singer from "../components/singer/singer.vue";
-import SingerDetail from "../components/singer-detail/singer-detail.vue";
-import TopList from "../components/top-list/top-list.vue"
-import UserCenter from "../components/user-center/user-center.vue"
-import Desc from "../components/desc/desc.vue"
+// import Rank from "../components/rank/rank.vue";
+// import Recommend from "../components/recommend/recomend.vue";
+// import Search from "../components/search/search.vue";
+// import Singer from "../components/singer/singer.vue";
+// import SingerDetail from "../components/singer-detail/singer-detail.vue";
+// import TopList from "../components/top-list/top-list.vue"
+// import UserCenter from "../components/user-center/user-center.vue"
+// import Desc from "../components/desc/desc.vue"
+const  Rank = () => import("../components/rank/rank.vue")
+const  Recommend = () => import("../components/recommend/recomend.vue")
+const  Search = () => import("../components/search/search.vue")
+const  Singer = () => import("../components/singer/singer.vue")
+const  SingerDetail = () => import("../components/singer-detail/singer-detail.vue")
+const  TopList = () => import("../components/top-list/top-list.vue")
+const  UserCenter = import("../components/user-center/user-center.vue")
+const  Desc = () => import("../components/desc/desc.vue")
 
 Vue.use(Router)
 
